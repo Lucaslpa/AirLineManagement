5ebaa569e03f0a51697d7326405c41812e1cf601
lucas <lucaslpa12345@gmail.com>
2024-01-07 19:59:22 -0300

    Test: Adicionando teste dos validadores de cpf, cnpj e email

---
+++ AirLineManagement.domain.tests/Validators/CpfCnpjValidatorTests.cs
@@ -0,0 +1,68 @@
+﻿
+
+using Bogus;
+using Bogus.Extensions.Brazil;
+using Testes.domain.Validators;
+
+namespace AirLineManagement.tests.Validators
+{
+    public class CpfCnpjValidatorTests
+    {
+
+        [Fact]
+        public void Should_return_false_when_cpf_is_invalid()
+        {
+            //Arrange
+            var cpf = "342535345";
+
+            //Act
+            var result = CpfCnpjValidator.CpfIsValid( cpf );
+
+            //Assert
+            Assert.False( result );
+        }
+
+        [Fact]
+        public void Should_return_true_when_cpf_is_valid()
+        {
+            //Arrange
+            var faker = new Faker( "pt_BR" );
+            var cpf = faker.Person.Cpf();
+
+            //Act
+            var result = CpfCnpjValidator.CpfIsValid( cpf );
+
+            //Assert
+            Assert.True( result );
+        }
+
+        [Fact]
+        public void Should_return_false_when_cnpj_is_invalid()
+        {
+            //Arrange
+            var cnpj = "342535345";
+
+            //Act
+            var result = CpfCnpjValidator.CnpjIsValid( cnpj );
+
+            //Assert
+            Assert.False( result );
+        }
+
+        [Fact]
+        public void Should_return_true_when_cnpj_is_valid()
+        {
+            //Arrange
+            var faker = new Faker( "pt_BR" );
+            var cnpj = faker.Company.Cnpj();
+
+            //Act
+            var result = CpfCnpjValidator.CnpjIsValid( cnpj );
+
+            //Assert
+            Assert.True( result );
+        }
+
+
+    }
+}
---
+++ AirLineManagement.domain.tests/Validators/PhoneValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using Bogus;
+using Testes.domain.Validators;
+
+namespace AirLineManagement.tests.Validators
+{
+    public class PhoneValidatorTests
+    {
+        [Fact]
+        public void Should_Return_True_When_Phone_Is_Valid()
+        {
+
+            // Arrange
+            var faker = new Faker( "pt_BR" );
+            var phone = faker.Phone.PhoneNumber();
+
+            // Act
+            var result = PhoneValidator.IsValid( phone );
+
+            // Assert
+            Assert.True( result );
+        }
+
+        [Fact]
+        public void Should_Return_False_When_Phone_Is_Invalid()
+        {
+            // Arrange
+            var phone = "11999999";
+
+            // Act
+            var result = PhoneValidator.IsValid( phone );
+
+            // Assert
+            Assert.False( result );
+        }
+    }
+}
