d2c5431fdee2eecb32f98afdf30d8ccd9ebd57db
lucas <lucaslpa12345@gmail.com>
2024-01-07 20:55:58 -0300

    Tests: Criando test de repositorio da companhoa, metodo adicionar companhia.

---
+++ AirLineManagement.domain.tests/Services/CompanyServicesTests.cs
@@ -0,0 +1,37 @@
+﻿
+
+using AirLineManagement.domain.Services;
+using Moq;
+using Testes.domain.Interfaces;
+using Testes.domain.Interfaces.Repositories;
+using Testes.domain.Model;
+using Testes.domain.tests.Fixtures;
+
+namespace AirLineManagement.tests.Services
+{
+    [Collection(nameof( CompanyFixtureCollection ) )]
+    public class CompanyServicesTests(CompanyTestsFixture companyTestsFixture)
+    {
+
+        private readonly CompanyTestsFixture _companyFixture = companyTestsFixture;
+
+
+        [Fact]
+       public async Task Should_add_company_correctly()
+        {
+            //Arrange
+            var company = _companyFixture.CreateValidCompany();
+            var mockCompanyRepository = new Mock<ICompanyRepository>();
+            var mockNotifier = new Mock<INotifier>(); 
+            var companyService = new CompanyService(mockCompanyRepository.Object, mockNotifier .Object);
+
+            //act
+            var result = await companyService.Add(company);
+
+            //Assert
+            mockCompanyRepository.Verify(r => r.Add(company), Times.Once);
+            Assert.True(result);
+        }
+
+    }
+}
