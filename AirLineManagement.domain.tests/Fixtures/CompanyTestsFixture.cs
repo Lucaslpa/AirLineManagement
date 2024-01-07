8b8b70de8faeeb24abcb3790a261aabdb68b3e9d
lucas <lucaslpa12345@gmail.com>
2024-01-07 20:57:50 -0300

    feat: criando metodo de criar compania invalida

--- AirLineManagement.domain.tests/Fixtures/CompanyTestsFixture.cs
+++ AirLineManagement.domain.tests/Fixtures/CompanyTestsFixture.cs
@@ -5,15 +5,32 @@ using Testes.domain.Model;
 
 namespace Testes.domain.tests.Fixtures
 {
+
+    [CollectionDefinition( nameof( CompanyFixtureCollection ) )]
+    public class CompanyFixtureCollection : ICollectionFixture<CompanyTestsFixture>
+    {
+    }
+
     public class CompanyTestsFixture : IDisposable
     {
         public  Company CreateValidCompany()
         {
             var faker = new Faker( "pt_BR" );
-            return new Company( faker.Company.CompanyName(), 
+            return new Company( faker.Company.CompanyName() + faker.Company.CompanySuffix() , 
                                 faker.Company.Cnpj(), 
                                 faker.Address.StreetAddress(),
-                                 faker.Phone.PhoneNumber(),
+                                 faker.Phone.PhoneNumberFormat(),
+                                 faker.Internet.Email()
+                                 );
+        }
+
+        public Company CreateInvalidCompany()
+        {
+            var faker = new Faker( "pt_BR" );
+            return new Company( faker.Company.CompanyName() ,
+                                faker.Company.Cnpj() ,
+                                faker.Address.StreetAddress() ,
+                                 faker.Phone.PhoneNumber() ,
                                  faker.Internet.Email()
                                  );
         }
